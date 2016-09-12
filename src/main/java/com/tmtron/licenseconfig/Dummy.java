4c72a9d47b31e2192ad447c80174b967f823e1d4
martin.trummer <martin.trummer@tmtron.com>
2016-09-12 11:37:06 +0000

    release V1.0.0 * prepared pom.xml for release * added Dummy.java file so that we can generate -sources and -javadoc jars (which are required to release the project to the public maven repository)

---
+++ src/main/java/com/tmtron/licenseconfig/Dummy.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2016 Martin Trummer (martin.trummer@tmtron.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tmtron.licenseconfig;
+
+/**
+ * This dummy class is never used.
+ * <p>
+ * It only exists, so that we can easily provide -sources and -javadoc jar files,
+ * which are required for uploading the project to maven central.
+ * </p>
+ * <p>
+ *     see: <a href="http://central.sonatype.org/pages/requirements.html#supply-javadoc-and-sources">Supply Javadoc and Sources</a>
+ * </p>
+ */
+public class Dummy {
+}
